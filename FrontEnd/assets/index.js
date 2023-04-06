17644219c1768ada0980523548736f51badd511e
Blidnight <jeansaintfleur34@gmail.com>
2023-04-06 04:52:00 -0400

    fetch gallery image for the modal

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -1,11 +1,99 @@
 /** API Request functions **/
 
+let modalOpen = '';
+
+
 async function apiFetch(path, options = { headers : { accept: 'application/json' }}) {
     const req = await fetch(`http://localhost:5678/api/${path}`, options);
     const data = await req.json();
 
     return data;
 }
+
+const MODAL_HTML_STRUCTURE = `
+    <div class="modal">
+        <div class="modal-actions">
+            <div class="modal-back-button">
+                <i class="fa-solid fa-arrow-left"></i>
+            </div>
+            <div class="modal-close-button"><i class="fa-solid fa-xmark"></i></div>
+        </div>
+        <div class="modal-body">
+            <h2 class="modal-title">Modal Title</h2>
+            <div class="modal-content">
+                
+            </div>
+            <div class="modal-body-actions">
+                
+            </div>
+        </div>
+    </div>
+`;
+
+
+async function openModal(modalName) {
+    const modalInPage = document.querySelector('.modal-backdrop');
+    if (modalInPage !== null) {
+        modalInPage.remove();
+    }
+
+    if (modalName === 'home') {
+        const actions = [
+            {
+                label : 'Ajouter une photo',
+                cssClass : 'modal-button-primary',
+                action: function () {
+
+                }
+            },
+            {
+                label: 'Supprimer la galerie',
+                cssClass : 'modal-button-secondary',
+                action: function () {
+                    
+                }
+            }
+        ];
+        const modal = document.createElement('div');
+        modal.classList.add('modal-backdrop');
+        modal.innerHTML = MODAL_HTML_STRUCTURE;
+
+        const modalTitle = modal.querySelector('h2.modal-title');
+        modalTitle.textContent = 'Galerie photo';
+
+        document.body.appendChild(modal);
+
+        const worksData = await apiFetch('works');
+        const modalContent = modal.querySelector('.modal-content');
+        const galleryPhotos = document.createElement('div');
+        galleryPhotos.classList.add('gallery-photos');
+        modalContent.appendChild(galleryPhotos);
+
+        const GALLERY_PHOTO_STRUCTURE = `
+            <div class="gallery-photo-actions">
+                <div class="move"><i class="fa-solid fa-up-down-left-right"></i></div>
+                <div class="delete"><i class="fa-solid fa-trash"></i></div>
+            </div>
+            <div class="gallery-image">
+                <img src="" alt="">
+            </div>
+            <div class="edit">éditer</div>
+        `;
+
+        for (let work of worksData) {
+            const galleryPhoto = document.createElement('div');
+            galleryPhoto.classList.add('gallery-photo');
+            galleryPhoto.innerHTML = GALLERY_PHOTO_STRUCTURE;
+            const galleryImage = galleryPhoto.querySelector('.gallery-image img');
+            galleryImage.src = work.imageUrl;
+            galleryImage.alt = work.title;
+            galleryPhotos.appendChild(galleryPhoto);
+        }
+
+        
+    }
+}
+
 
 /** Filter function */
 
@@ -82,6 +170,8 @@ async function initializeProject() {
     ];
     let activeFilterId = -1;
 
+    gallery.innerHTML = '';
+
     for (let work of worksData) {
         const workFigure = document.createElement('figure');
         workFigure.setAttribute('category-id', work.categoryId);
@@ -99,6 +189,7 @@ async function initializeProject() {
         gallery.appendChild(workFigure);
     }
 
+    galleryFilter.innerHTML = '';
     for (let category of categoriesData) {
         const categoryLi = document.createElement('li');
         const categoryButton = document.createElement('button');
